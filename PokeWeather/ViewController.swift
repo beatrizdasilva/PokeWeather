4e03f75d0aa70019cbfd74bbb1ef83a548ebf7a8
ju-machado <61711864+ju-machado@users.noreply.github.com>
2021-02-23 16:04:02 -0300

    refactor: removed unnecessary function

--- PokeWeather/ViewController.swift
+++ PokeWeather/ViewController.swift
@@ -12,23 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        requisicao()
     }
     
-    func requisicao(){
-        if let url = URL(string: "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139"){
-            URLSession.shared.dataTask(with: url){data, response, error in
-                if let data = data{
-                    do{
-                        let response = try JSONDecoder().decode(WeatherResponse.self, from: data)
-                        print(response)
-                    }
-                    catch{
-                        print(error)
-                    }
-                }
-            }.resume()
-        }
-    }
 }
 
