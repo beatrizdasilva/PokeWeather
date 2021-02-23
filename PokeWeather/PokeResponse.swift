ded1587d37dbc9d7bbb35cd3ff6fd09b53a1335e
ju-machado <61711864+ju-machado@users.noreply.github.com>
2021-02-23 15:59:07 -0300

    feat: Requisition base structures for PokeAPI and WeatherAPI

---
+++ PokeWeather/PokeResponse.swift
@@ -0,0 +1,33 @@
+//
+//  PokeResponse.swift
+//  PokeWeather
+//
+//  Created by Juliana Pereira Machado on 23/02/21.
+//
+
+import Foundation
+
+struct Species: Codable {
+    let name: String
+}
+
+struct Sprites: Codable {
+    let front_default: String
+}
+
+struct Types: Codable {
+    let type: Type
+}
+
+struct Type: Codable {
+    let name: String
+}
+
+struct PokeResponse: Codable {
+    let species: Species
+    let sprites: Sprites
+    let types: [Types]
+    let weight: Int
+}
+
+
---
+++ PokeWeather/WeatherResponse.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherResponse.swift
+//  PokeWeather
+//
+//  Created by Juliana Pereira Machado on 23/02/21.
+//
+
+import Foundation
+
+struct Weather: Codable{
+    let main: String
+}
+
+struct WeatherResponse: Codable {
+    let weather: [Weather]
+    let name: String
+}
