de2c0f47eaf0fc35ad9b96672f205fb95d7cc74d
kahbyte <kaue.sbsousa@gmail.com>
2021-03-02 18:44:57 -0300

    feature: resolved merge problem

---
+++ PokeWeather/ViewController/TeamViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TeamViewController.swift
+//  PokeWeather
+//
+//  Created by Beatriz da Silva on 01/03/21.
+//
+import UIKit
+
+class TeamViewController: UIViewController {
+    @IBOutlet weak var PokemonCard: TeamPokemonView!
+    
+    @IBOutlet weak var firstPokemonMiniature: UIImageView!
+    @IBOutlet weak var secondPokemonMiniature: UIImageView!
+    @IBOutlet weak var thirdPokemonMiniature: UIImageView!
+    
+    @IBOutlet weak var firstPokemonArrow: UIImageView!
+    @IBOutlet weak var secondPokemonArrow: UIImageView!
+    @IBOutlet weak var thirdPokemonArrow: UIImageView!
+    
+    @IBOutlet weak var shareYourTeam: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        PokemonCard.layer.cornerRadius = PokemonCard.frame.height / 2
+    }
+
+}
